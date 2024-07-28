c4553ee9391074b31b15dc419e7a047dd98e3768
nattolecats <34679325+nattolecats@users.noreply.github.com>
2024-07-28 23:20:19 +0530

    ivan:props:disable adb secure

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -157,6 +157,7 @@ BOARD_SUPER_PARTITION_GROUPS := main
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
 
 # Power
