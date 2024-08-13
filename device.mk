6febc270b5a858fb4afff0d028d86b1a38b44de5
Mashopy <eliasgheeraert@gmail.com>
2024-08-13 10:58:55 +0000

    ivan:Switch to regular QTI vibrator

--- device.mk
+++ device.mk
@@ -445,8 +445,13 @@ PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.oplus
 
 # Vibrator
+$(call soong_config_set, vibrator, vibratortargets, vibratoraidlV2target)
+
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator-service.mt6877
+    vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
 PRODUCT_PACKAGES += \
