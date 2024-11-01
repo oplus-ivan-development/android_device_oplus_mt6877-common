a2c2898982cf658272556615fe555885a60988a7
electimon <electimon@gmail.com>
2024-11-01 08:47:53 +0530

    ivan:switched to prebuilt sensor service

--- device.mk
+++ device.mk
@@ -393,12 +393,11 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0.vendor \
-    android.hardware.sensors@2.1.vendor \
-    android.frameworks.sensorservice@1.0 \
+    android.hardware.sensors@2.0.vendor:64 \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@2.0-service-multihal.mt6877 \
-    libsensorndkbridge
+    libsensorndkbridge \
+    libpower.vendor:64 \
+    libshim_sensors
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
